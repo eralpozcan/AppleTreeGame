cb7305119e812d0d24ffd2c44960a028518eead0
Eralpozcan <eralpozcans@gmail.com>
2022-10-20 17:19:54 +0300

    feat: create main-screen and play button

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <script setup>
 import AppleTree from './components/AppleTree.vue'
+import MainScreen from './components/MainScreen.vue'
 </script>
 
 <template>
   <div id="app">
-    <AppleTree />
+    <MainScreen />
   </div>
 </template>
 
 <style scoped>
-
+.body{
+  background-color: #516dff;
+  color: #fff;
+  font-family: 'Press Start 2P', sans-serif;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+  text-align: center;
+}
 
 </style>
---
+++ src/components/MainScreen.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+
+
+<template>
+    <main class="main-screen">
+        <h1>Play to Apple Tree Game</h1>
+        <button class="home-button"> Play Button</button>
+    </main>
+</template>
+
+
+
+<style scoped>
+.main-screen{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  transition: margin 0.5s ease-out;  
+}
+
+.home-button{
+  border: 0;
+  background-color: #fff;
+  color: #516dff;
+  padding: 15px 20px;
+  font-family: inherit;
+  cursor: pointer;
+}
+
+
+</style>
